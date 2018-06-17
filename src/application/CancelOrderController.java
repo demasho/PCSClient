3a143c2053ac0932d4b674284e4b29faf832d2c5
aaburafe <aehm97@gmail.com>
2018-06-17 23:56:57 +0300

    a

--- src/application/CancelOrderController.java
+++ src/application/CancelOrderController.java
@@ -1,17 +1,35 @@
 package application;
 
+import client.ChatClient;
+import client.ClientConsole;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class CancelOrderController {
 
-    @FXML
-    private Button cancelButton;
+	@FXML
+	private TextField order_id;
 
-    @FXML
-    void CancelButtonAction(ActionEvent event) {
-    	AlertBox.display("Loading", "Loading .....", "Please Wait");
-    }
+
+	@FXML
+	private Button cancelButton;
+	
+	private ClientConsole console = ClientConsole.getInstance();
+	private ChatClient client=console.getClient();
+
+	@FXML
+	void CancelButtonAction(ActionEvent event) {
+		String order;
+		StringBuilder sent = new StringBuilder();
+		sent.append("CANCEL_RESERVATION : ");
+		order=order_id.getText().trim();
+		sent.append(order);
+		AlertBox.display("ביטול חניה", "נא לחכות", "העניין בטיפול");
+		client.handleMessageFromClientUI(sent.toString());
+		AlertBox.display("ביטול חניה", "הביטול התבצע בהצלחה", "תודה ולהתראות");
+		
+	}
 
 }
